814ac187c6eba2e9189cf90d071a16034ffdc063
marlonsmathias <marlonsmathias@gmail.com>
2021-08-05 22:03:10 -0300

    First working version

--- transcriPy.py
+++ transcriPy.py
@@ -1,10 +1,9 @@
-import speech_recognition as sr
+import vosk
 import tkinter.filedialog
 import tkinter as tk
 import os
-import re
-from math import floor,ceil
-import traceback
+import subprocess
+from math import floor
 
 # TODO: Add other speech recognition servers
 # TODO: Add language selector
@@ -29,21 +28,6 @@ def initialMessage():
     print('You should have received a copy of the GNU General Public License')
     print('along with this program.  If not, see <https://www.gnu.org/licenses/>')
     print('')
-
-def getKey(filePath = './key.txt'):
-    try:
-        with open(filePath, "r") as f:
-            s = f.readline().replace("\n","").split()
-            apiKey = s[0]
-            if len(s) > 1:
-                api = s[1]
-            else:
-                api = 'wit'
-        return api, apiKey
-    except:
-        print(f"Nenhuma chave de API foi encontrada no arquivo {filePath}.\nA chave para o wit.ai pode ser gerada em <https://wit.ai/>. É necessário fazer login.")
-        print('O serviço do Google será usado com chave padrão, que deve ser usada apenas para fins de teste e uso pessoal.\nO programa funcionará normalmente.')
-        return 'google', None
 
 def getFileName():
     root = tk.Tk()
@@ -77,94 +61,48 @@ def checkffmpeg():
             input('Aperte Enter para sair')
             exit()
 
-
-def getBlocks(filepath,minLength=0.3,maxLength=10):
-    os.system(f"ffmpeg -i \"{filepath}\" -af silencedetect=noise=-30dB:d=0.5 -f null - 2> temp.txt")
-    starts = [0]
-    durations = []
-
-    with open('temp.txt', encoding='utf-8') as f:
-        for line in f:
-            if 'silence_duration:' in line:
-                numbers = re.findall("[-+]?\d*\.\d+|\d+",line)
-                silenceEnd = float(numbers[-2])
-                silenceDuration = float(numbers[-1])
-                durations.append(silenceEnd-silenceDuration-starts[-1])
-                starts.append(silenceEnd)
-            if 'time=' in line:
-                for chunk in line.split():
-                    if 'time=' in chunk:
-                        numbers = re.findall("[-+]?\d*\.\d+|\d+",line)
-                        end = 3600*int(numbers[0]) + 60*int(numbers[1]) + float(numbers[2])
-
-    os.remove('./temp.txt')
-
-    durations.append(end-starts[-1])
-
-    blocks = []
-    for i in range(0,len(starts)):
-        s = starts[i]
-        d = durations[i]
-        if d>minLength and d<=maxLength:
-            blocks.append([s, d])
-        if d>maxLength:
-            n = ceil(d/maxLength)
-            for j in range(0,n):
-                blocks.append([s+j*d/n, d/n])
-
-    return blocks
-
-def process(filepath,blocks,api='wit',apiKey=None):
+def process(filepath,modelFolder,sampleRate):
 
     # Get output file
     outName = filepath[:-4] + '-transcrito.txt'
 
-    recognizer = sr.Recognizer()
-    
-    nBlocks = len(blocks)
+    # Initialize model
+    vosk.SetLogLevel(-1)
+    model = vosk.Model(modelFolder)
+    recognizer = vosk.KaldiRecognizer(model,sampleRate)
 
-    # Now process each chunk
-    for i in range(0,nBlocks):
+    # Read audio with ffmpeg
+    ffmpegProcess = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i', filepath, '-ar', str(sampleRate) , '-ac', '1', '-f', 's16le', '-'], stdout=subprocess.PIPE)
 
-        s = blocks[i][0]
-        d = blocks[i][1]
-        tStr = f"{floor(s/3600)}:{floor(s/60):02}:{s%60:04.1f}"
+    # Get terminal size
+    width = os.get_terminal_size()[0]
 
-        print(f"Processando bloco {i+1} de {nBlocks} ({tStr})")
-        
-        # Split chunk with ffmpeg
-        os.system(f"ffmpeg -hide_banner -loglevel error -y -ss {s} -i \"{filepath}\" -t {d} temp.wav")
+    time = 0
+    dt = 8*4000/(16*sampleRate)
+    print(f'[{floor(time/60)}:{floor(time%60):02}]')
 
-        # Load chunk
-        with sr.AudioFile('./temp.wav') as source:
-            audio = recognizer.record(source)
-        try:
-            if api=='wit':
-                transcript = recognizer.recognize_wit(audio, key=apiKey)
-            else:
-                transcript = recognizer.recognize_google(audio, key=apiKey, language='pt-BR')
-        except:
-            print("Erro no bloco:")
-            traceback.print_exc()
-            transcript = ''
-
-        print(transcript)
-
-        # Write output to file
-        with open(outName, "a") as outHandle:
-            outHandle.write(f'{tStr}\n')
-            outHandle.write(f'{transcript}\n')
-
-    # Clean up temporary file
-    os.remove('./temp.wav')
+    # Process chunk
+    while True:
+        time += dt
+        data = ffmpegProcess.stdout.read(4000)
+        if len(data) == 0:
+            break
+        if recognizer.AcceptWaveform(data):
+            t = recognizer.Result()
+            t = t.split('"')[3]
+            print(t)
+            print(f'[{floor(time/60)}:{floor(time%60):02}]')
+            with open(outName, "a") as outHandle:
+                outHandle.write(f'{t}\n')
+        else:
+            t = recognizer.PartialResult()
+            t = t.split('"')[3]
+            print(t[-width:-1], end='\r')
 
 # Main part
 if __name__ == "__main__":
     # Aviso inicial
     initialMessage()
-
-    # Get wit.ai API key
-    api,apiKey = getKey()
 
     # Get file name
     filepath = getFileName()
@@ -174,8 +112,5 @@ if __name__ == "__main__":
     # Check ffmpeg
     checkffmpeg()
 
-    # Get audio blocks
-    blocks = getBlocks(filepath,maxLength=10)
-
     # Process file
-    process(filepath,blocks,api=api,apiKey=apiKey)
+    process(filepath,modelFolder='modelo-portugues',sampleRate=16000)
